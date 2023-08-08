80de11bd32b6aeb075a1bb458c118e86579294d2
bartlomiej-gora <bartlomiej.gora@gmail.com>
2023-08-08 20:49:14 +0200

    resilience added

---
+++ FallBackService/src/main/java/com/gihtub/bartgora/fallbackservice/FallBackServiceApplication.java
@@ -0,0 +1,13 @@
+package com.gihtub.bartgora.fallbackservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FallBackServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FallBackServiceApplication.class, args);
+	}
+
+}
