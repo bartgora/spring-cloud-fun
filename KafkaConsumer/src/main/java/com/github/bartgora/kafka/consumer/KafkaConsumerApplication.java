3be255f45c368694d3732edd00bd9e21926d22fc
bartlomiej-gora <bartlomiej.gora@gmail.com>
2023-09-04 21:23:15 +0200

    Fun With Kafka. Metrics added

--- KafkaConsumer/src/main/java/com/github/bartgora/kafka/consumer/KafkaConsumerApplication.java
+++ KafkaConsumer/src/main/java/com/github/bartgora/kafka/consumer/KafkaConsumerApplication.java
@@ -1,5 +1,9 @@
 package com.github.bartgora.kafka.consumer;
 
+import io.micrometer.core.instrument.Counter;
+import io.micrometer.core.instrument.MeterRegistry;
+import io.micrometer.core.instrument.Metrics;
+import io.micrometer.core.instrument.simple.SimpleMeterRegistry;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -8,6 +12,7 @@ import org.springframework.kafka.annotation.KafkaListener;
 @SpringBootApplication
 public class KafkaConsumerApplication {
 
+	static final Counter counter = Metrics.counter("kafka.incoming.messages");
 	public static void main(String[] args) {
 		SpringApplication.run(KafkaConsumerApplication.class, args);
 	}
@@ -15,6 +20,7 @@ public class KafkaConsumerApplication {
 
 	@KafkaListener(topics = "kafka-fun", groupId = "fun-group" )
 	public void receive(Game game){
+		counter.increment();
 		System.out.println("Game: " + game.getTitle() +" Genre: " + game.getGenre());
 	}
 }
