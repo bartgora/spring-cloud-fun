bec113c514174eb79f84d32694bb12ca697ff29e
bartlomiej-gora <bartlomiej.gora@gmail.com>
2024-01-05 18:03:09 +0100

    Prometheus works

--- HelloService/src/main/java/com/github/bartgora/helloservice/Controller.java
+++ HelloService/src/main/java/com/github/bartgora/helloservice/Controller.java
@@ -2,7 +2,6 @@ package com.github.bartgora.helloservice;
 
 import io.micrometer.core.instrument.Counter;
 import io.micrometer.core.instrument.MeterRegistry;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
